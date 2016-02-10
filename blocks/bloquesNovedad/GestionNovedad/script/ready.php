2ecd8f9a7c1e912520a9d24296624b13992ff31e
julian7716 <juliant7@hotmail.com>
2016-02-10 12:53:40 -0500

    se agrega los select para el formulario inicial de gestion de novedades

--- blocks/bloquesNovedad/GestionNovedad/script/ready.php
+++ blocks/bloquesNovedad/GestionNovedad/script/ready.php
@@ -14,17 +14,16 @@ $('#datepicker').datepicker({
 	autoHidePrompt: true
 });
 
-$('#<?php echo $this->campoSeguro('fdpDepartamento')?>').width(200); 
-$("#<?php echo $this->campoSeguro('fdpDepartamento')?>").select2();
+$('#<?php echo $this->campoSeguro('tipoNovedad')?>').width(200); 
+$("#<?php echo $this->campoSeguro('tipoNovedad')?>").select2();
 
-$('#<?php echo $this->campoSeguro('fdpCiudad')?>').width(200); 
-$("#<?php echo $this->campoSeguro('fdpCiudad')?>").select2();
+$('#<?php echo $this->campoSeguro('categoriaConceptos')?>').width(200); 
+$("#<?php echo $this->campoSeguro('categoriaConceptos')?>").select2();
 
-$('#<?php echo $this->campoSeguro('concepto')?>').width(200); 
-$("#<?php echo $this->campoSeguro('concepto')?>").select2();
+$('#<?php echo $this->campoSeguro('ley')?>').width(200); 
+$("#<?php echo $this->campoSeguro('ley')?>").select2();
 
-$('#<?php echo $this->campoSeguro('tipoVinculacion')?>').width(200); 
-$("#<?php echo $this->campoSeguro('tipoVinculacion')?>").select2();
+$('#<?php echo $this->campoSeguro('naturaleza')?>').width(200); 
+$("#<?php echo $this->campoSeguro('naturaleza')?>").select2();
 
-$('#<?php echo $this->campoSeguro('tipoNomina')?>').width(200); 
-$("#<?php echo $this->campoSeguro('tipoNomina')?>").select2();
+
